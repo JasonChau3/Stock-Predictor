81bb2cef1405a83295b26b615a5d7692af999541
JasonChau3 <38936513+JasonChau3@users.noreply.github.com>
2021-01-11 20:45:45 -0800

    created the data of the stocks

--- webscraper.py
+++ webscraper.py
@@ -2,12 +2,20 @@ import yfinance as yf
 import bs4 as bs
 import pickle
 import requests
+import pandas as pd
 from getTickers import *
+import os
 
+from pandas_datareader import data as pdr
 
+yf.pdr_override()
 dow = save_dow_tickers()
 print(dow)
     
-data = yf.download(dow,period = "6mo", group_by='ticker')
-print(data)
+data = pdr.get_data_yahoo(dow,period = "6mo", group_by='ticker')
+
+
+for tickers in dow:
+    df= pd.DataFrame(data[tickers])
+    df.to_excel(tickers +'.xlsx')
 
