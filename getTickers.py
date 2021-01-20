d8d3082b929d5c4522c098d762877ca491b346e5
JasonChau3 <38936513+JasonChau3@users.noreply.github.com>
2021-01-20 10:47:11 -0800

    added sp500 data

--- getTickers.py
+++ getTickers.py
@@ -1,6 +1,7 @@
 import bs4 as bs 
 import pickle
 import requests
+import pandas as pd
 import re
 
 def save_dow_tickers():
@@ -16,4 +17,16 @@ def save_dow_tickers():
     
     return tickers
 
+def save_sp500_tickers():
+    data = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    table = data[0]
+    print(table.head())
+    print(table.columns)
+    #sliced_table = table[1:]
+    #header = table.iloc[0]
+    #corrected_table = sliced_table.rename(columns=header)
+    tickers = list(table[1:]['Symbol'])
+    return tickers
 
+
+print(save_sp500_tickers())
--- webscraper.py
+++ webscraper.py
@@ -8,6 +8,7 @@ import os
 
 from pandas_datareader import data as pdr
 
+
 yf.pdr_override()
 dow = save_dow_tickers()
 print(dow)
@@ -17,5 +18,20 @@ data = pdr.get_data_yahoo(dow,period = "6mo", group_by='ticker')
 
 for tickers in dow:
     df= pd.DataFrame(data[tickers])
-    df.to_excel('./data' +tickers +'.xlsx')
+    df.to_excel('./dowJonesData/' +tickers +'.xlsx')
 
+print('helo');
+sp500tic = save_sp500_tickers()
+print(sp500tic)
+
+data = pdr.get_data_yahoo(sp500tic,period = "6mo", group_by='ticker')
+
+for tickers in sp500tic:
+    df= pd.DataFrame(data[tickers])
+    df.to_excel('./SP500Data/' +tickers +'.xlsx')
+
+tickerdf = pd.DataFrame(sp500tic,columns=['ticker'])
+#sectordf = pd.DataFrame(sp50Industry,columns=['industry'])
+
+#tickerandsector = pd.concat([tickerdf, sectordf], axis=1,join = tickerdf.index)
+#tickerandsector.to_excel('./SP500Data/Sp500Industries.xlsx')
