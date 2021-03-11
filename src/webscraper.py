8e548a1c256beb52050312e5ae80d5b1b444c6d4
JasonChau3 <jac296@ucsd.edu>
2021-03-11 01:45:59 -0800

    added 12 mo

--- src/webscraper.py
+++ src/webscraper.py
@@ -31,18 +31,18 @@ def getData():
         
         df['Date'] = df.index.format()
 
-        df.to_csv('../data/dowJonesData/' +tickers +'.csv',index= False);
+        df.to_csv('./data/dowJonesData/' +tickers +'.csv',index= False);
 
     sp500tic = save_sp500_tickers()
     data = pdr.get_data_yahoo(sp500tic,period = "6mo", group_by='ticker')
 
     #loops for each ticker and create a dataaframe out of it
-    #for tickers in sp500tic:
-    #    df= pd.DataFrame(data[tickers])
-    #    df = (df-df.min())/(df.max()-df.min())
-    #    df = df.iloc[1:]
-    #    df['Date'] = df.index.format()
-    #    df.to_csv('./data/SP500Data/' +tickers +'.csv', index = False)
+    for tickers in sp500tic:
+        df= pd.DataFrame(data[tickers])
+        df = (df-df.min())/(df.max()-df.min())
+        df = df.iloc[1:]
+        df['Date'] = df.index.format()
+        df.to_csv('./data/SP500Data/' +tickers +'.csv', index = False)
 
     #tickerdf = pd.DataFrame(sp500tic,columns=['ticker'])
 
